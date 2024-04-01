66fd5e4c2456b1e49c9144ccdfaebacf0bad419b
Tran Chau Phu <ilovewhenifyoucallme@gmail.com>
2024-04-01 11:26:13 +0000

    Merge branch '0104' into 'master'
    BNTV khoa 17 DichVu
    
    See merge request quoclongdng/17_be!1

---
+++ app/Http/Controllers/DichVuController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DichVu;
+use Illuminate\Http\Request;
+
+class DichVuController extends Controller
+{
+    public function getData()
+    {
+        $data   =   DichVu::all();
+
+        return response()->json([
+            'dich_vu'  =>  $data
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $data   =   $request->all();
+        DichVu::create($data);
+
+        return response()->json([
+            'status'    =>  true,
+            'message'   =>  'Đã tạo mới dịch vụ thành công!'
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        DichVu::find($id)->delete();
+
+        return response()->json([
+            'status'    =>  true,
+            'message'   =>  'Đã xoá dịch vụ thành công!'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DichVuController;
 use App\Http\Controllers\LoaiPhongController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,8 @@ Route::get('/loai-phong/data', [LoaiPhongController::class, 'getData']);
 Route::post('/loai-phong/create', [LoaiPhongController::class, 'store']);
 Route::delete('/loai-phong/delete/{id}', [LoaiPhongController::class, 'destroy']);
 // 127.0.0.1:8000/api/loai-phong/data
+
+
+Route::get('/dich-vu/data', [DichVuController::class, 'getData']);
+Route::post('/dich-vu/create', [DichVuController::class, 'store']);
+Route::delete('/dich-vu/delete/{id}', [DichVuController::class, 'destroy']);
